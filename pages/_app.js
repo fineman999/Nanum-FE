3be0f202d05632217fb546ae8814d8cff63ca932
chanyeong <82819756+chyg-ai@users.noreply.github.com>
2022-09-23 15:27:12 +0900

    [#2] feat : 하우스 리스트 수정 및 좋아요 버튼 추가

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import "../styles/global.css";
 // Swiper.js
 import "swiper/css";
 import "swiper/css/effect-fade";
-import "swiper/css/pagination";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
