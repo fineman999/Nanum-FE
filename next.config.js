0b9b1e7f6236d080867c1152719ea2cc59ea58d7
chanyeong <maxelto123@gmail.com>
2022-10-10 22:38:21 +0900

    [#28] fix : 개발 환경 수정

--- next.config.js
+++ next.config.js
@@ -8,6 +8,16 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com", "cdn-icons-png.flaticon.com"],
   },
+  experimental: {
+    modularizeImports: {
+      "@mui/material/?(((\\w*)?/?)*)": {
+        transform: "@mui/material/{{ matches.[1] }}/{{member}}",
+      },
+      "@mui/icons-material/?(((\\w*)?/?)*)": {
+        transform: "@mui/icons-material/{{ matches.[1] }}/{{member}}",
+      },
+    },
+  },
 };
 
 module.exports = nextConfig;
