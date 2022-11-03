81eafdfd1845c4264711e40daaeed31ec99c6202
chanyeong <82819756+chyg-ai@users.noreply.github.com>
2022-11-03 12:00:36 +0900

    [#93] feat : 하우스 검색 결과 리뷰평점 내림차순, 월세 최고가 오름/내림차순 정렬 기능 구현

--- state/atom/houseSearchFilterState.js
+++ state/atom/houseSearchFilterState.js
@@ -4,9 +4,10 @@ import { atom } from "recoil";
 // 남녀공용(COMMON)
 // 남성전용(MALE)
 // 여성전용(FEMALE)
-// 리뷰순(REVIEW)
-// 최저가(MINIMUM)
-// 최고가(MAXIMUM)
+// 리뷰 개수 오름/내림차순(REVIEW_CNT_ASCE, REVIEW_CNT_DESC)
+// 리뷰 평점 오름/내림차순(REVIEW_AVG_ASCE, REVIEW_AVG_DESC)
+// 월세 최저가 오름/내림차순 (MINIMUM_ASCE, MINIMUM_DESC)
+// 월세 최고가 오름/내림차순 (MAXIMUM_ASCE, MAXIMUM_DESC)
 // 좋아요(LIKE)
 
 const houseSearchFilterState = atom({
